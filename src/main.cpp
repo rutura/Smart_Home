b03c32b261184e8381b8e44727e467c14485189e
Koech <koech@novek.io>
2024-02-27 21:57:12 +0300

    f(permissions_and_themeing): added android template files

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,22 @@ int main(int argc, char *argv[])
   QGuiApplication app(argc, argv);
 
   QQmlApplicationEngine engine;
-#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
-  const QUrl url(u"qrc:/SmartHome/qml/MobileApp.qml"_qs);
-#else
-  const QUrl url(u"qrc:/SmartHome/qml/DesktopApp.qml"_qs);
-#endif
+
   QObject::connect(
-    &engine,
-    &QQmlApplicationEngine::objectCreationFailed,
-    &app,
-    []() { QCoreApplication::exit(-1); },
-    Qt::QueuedConnection);
-  engine.load(url);
+      &engine,
+      &QQmlApplicationEngine::objectCreationFailed,
+      &app,
+      []() { QCoreApplication::exit(-1); },
+      Qt::QueuedConnection);
+
+#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
+  // const QUrl url(u"qrc:/SmartHome/qml/MobileApp.qml"_qs);
+   engine.loadFromModule("SmartHome", "MobileApp");
+#else
+  // const QUrl url(u"qrc:/SmartHome/qml/DesktopApp.qml"_qs);
+   engine.loadFromModule("SmartHome", "DesktopApp");
+#endif
+  // engine.load(url);
 
   return app.exec();
 }
